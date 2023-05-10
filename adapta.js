1c99848ad947814507c8c990e09e6724eaad5342
gilpgdm <gilpgdm@gmail.com>
2023-05-10 16:12:56 -0600

    Agrega métodos asíncronos

--- adapta.js
+++ adapta.js
@@ -1,37 +1,100 @@
-const __cl = console.clear
-const __lo = console.log
-const __err = console.error
-const __pre = document.getElementById("__pre")
+const ___pre = document.getElementById("___pre")
+const ___lo = console.log
+const ___cl = console.clear
+const ___err = console.error
 window.onerror = (_evt, _src, lin, col, error) => {
- console.error((lin ? "Línea: " + (lin - 1) : ""), (col ? "columna: " + col : ""))
+ console.error((lin ? "Línea: " + (lin - 113) : ""), (col ? "columna: " + col : ""))
  console.error(error)
 };
 window.console.clear =
  /** @param {any[]} parametros */
  (...parametros) => {
-  __cl.apply(null, parametros)
-  if (__pre) {
-   __pre.textContent = ""
+  ___cl.apply(null, parametros)
+  if (___pre) {
+   ___pre.textContent = ""
   }
  }
 window.console.log =
  /** @param {any[]} parametros */
  (...parametros) => {
-  __lo.apply(null, parametros);
-  const div = document.createElement("div")
-  div.textContent = parametros.join(" ")
-  if (__pre) {
-   __pre.append(div);
+  ___lo.apply(null, parametros);
+  if (___pre) {
+   const div = ___pre.appendChild(document.createElement("div"))
+   div.textContent = parametros.join(" ")
   }
  };
 window.console.error =
  /** @param {any[]} parametros */
  (...parametros) => {
-  const div = document.createElement("div")
-  div.style.color = "red"
-  div.textContent = parametros.join(" ")
-  if (__pre) {
-   __pre.append(div)
+  ___err.apply(null, parametros);
+  if (___pre) {
+   const div = ___pre.appendChild(document.createElement("div"))
+   div.style.color = "red"
+   div.textContent = parametros.join(" ")
   }
-  __err.apply(null, parametros);
  }
+/**
+* @param {string} mensaje
+* @returns {Promise<string>}
+*/
+async function question(mensaje) {
+ const respuesta = prompt(mensaje)
+ return new Promise((resolve, reject) => {
+  try {
+   ___lo.call(null, "%c" + mensaje + respuesta, "color:blue;font-style:oblique");
+   if (___pre) {
+    const div = ___pre.appendChild(document.createElement("div"))
+    div.style.color = "blue"
+    div.style.fontStyle = "oblique"
+    div.textContent = mensaje + respuesta
+   }
+   setTimeout(() => resolve(respuesta || ""), 60)
+  } catch (e) {
+   reject(e)
+  }
+ })
+}
+/**
+ * @param {string} mensaje
+ */
+async function questionInt(mensaje) {
+ return parseInt(await question(mensaje) || "", 10)
+}
+/**
+ * @param {string} mensaje
+ */
+async function questionFloat(mensaje) {
+ return parseFloat(await question(mensaje) || "")
+}
+async function clear() {
+ return new Promise((resolve, reject) => {
+  try {
+   console.clear()
+   setTimeout(() => resolve(undefined), 60)
+  } catch (e) {
+   reject(e)
+  }
+ })
+}
+/** @param {any[]} parametros */
+async function log(...parametros) {
+ return new Promise((resolve, reject) => {
+  try {
+   console.log(...parametros);
+   setTimeout(() => resolve(undefined), 60)
+  } catch (e) {
+   reject(e)
+  }
+ })
+}
+/** @param {any[]} parametros */
+async function error(...parametros) {
+ return new Promise((resolve, reject) => {
+  try {
+   console.log(...parametros);
+   setTimeout(() => resolve(undefined), 60)
+  } catch (e) {
+   reject(e)
+  }
+ })
+}
--- index.js
+++ index.js
@@ -83,6 +83,7 @@ function ventanaSecActualiza() {
 
 function timeout() {
  setTimeout(ejecuta, 100)
+ return true
 }
 
 function ejecuta() {
@@ -154,12 +155,121 @@ async function registraServiceWorker() {
  * @param {boolean} expande
  */
 function codigoAdapta(src, expande) {
- return (/* html */ `<pre id="__pre"></pre><script>console.clear()</script>` +
-  (expande ?
-    /* html */ `<script>declara();debugger` :
-   `<script>debugger`)
-  + /* html */
-  `
+ const expansion = expande ? /* html */ `__declara();` : ""
+ return (/* html */
+  `<pre id="___pre"></pre>
+<script>
+ console.clear()
+ const ___pre = document.getElementById("___pre")
+ const ___lo = console.log
+ ${expansion}
+function __declara() {
+ const ___cl = console.clear
+ const ___err = console.error
+ window.onerror = (_evt, _src, lin, col, error) => {
+  console.error((lin ? "Línea: " + (lin - 113) : ""), (col ? "columna: " + col : ""))
+  console.error(error)
+ };
+ window.console.clear =
+  /** @param {any[]} parametros */
+  (...parametros) => {
+   ___cl.apply(null, parametros)
+   if (___pre) {
+    ___pre.textContent = ""
+   }
+  }
+ window.console.log =
+  /** @param {any[]} parametros */
+  (...parametros) => {
+   ___lo.apply(null, parametros);
+   if (___pre) {
+    const div = ___pre.appendChild(document.createElement("div"))
+    div.textContent = parametros.join(" ")
+   }
+  };
+ window.console.error =
+  /** @param {any[]} parametros */
+  (...parametros) => {
+   ___err.apply(null, parametros);
+   if (___pre) {
+    const div = ___pre.appendChild(document.createElement("div"))
+    div.style.color = "red"
+    div.textContent = parametros.join(" ")
+   }
+  }
+}
+/**
+* @param {string} mensaje
+* @returns {Promise<string>}
+*/
+async function question(mensaje) {
+ return new Promise((resolve, reject) => {
+  try {
+   const respuesta = prompt(mensaje)
+   try {
+    ___lo.call(null, "%c" + mensaje + respuesta, "color:blue;font-style:oblique");
+    if (___pre) {
+     const div = ___pre.appendChild(document.createElement("div"))
+     div.style.color = "blue"
+     div.style.fontStyle = "oblique"
+     div.textContent = mensaje + respuesta
+    }
+    setTimeout(() => resolve(respuesta || ""), 60)
+   } catch (e) {
+    reject(e)
+   }
+  } catch (e) {
+   reject(e)
+  }
+ })
+}
+/**
+ * @param {string} mensaje
+ */
+async function questionInt(mensaje) {
+ return parseInt(await question(mensaje) || "", 10)
+}
+/**
+ * @param {string} mensaje
+ */
+async function questionFloat(mensaje) {
+ return parseFloat(await question(mensaje) || "")
+}
+async function clear() {
+ return new Promise((resolve, reject) => {
+  try {
+   console.clear()
+   setTimeout(() => resolve(undefined), 60)
+  } catch (e) {
+   reject(e)
+  }
+ })
+}
+/** @param {any[]} parametros */
+async function log(...parametros) {
+ return new Promise((resolve, reject) => {
+  try {
+   console.log(...parametros);
+   setTimeout(() => resolve(undefined), 60)
+  } catch (e) {
+   reject(e)
+  }
+ })
+}
+/** @param {any[]} parametros */
+async function error(...parametros) {
+ return new Promise((resolve, reject) => {
+  try {
+   console.log(...parametros);
+   setTimeout(() => resolve(undefined), 60)
+  } catch (e) {
+   reject(e)
+  }
+ })
+}
+</script>
+<script>__ejecuta();async function __ejecuta(){try {
+debugger;
 ${src}
 
 
@@ -201,103 +311,42 @@ ${src}
 
 
 
-function declara() {
- const __cl = console.clear
-const __lo = console.log
-const __err = console.error
-const __pre = document.getElementById("__pre")
-window.onerror = (_evt, _src, lin, col, error) => {
- console.error((lin ? "Línea: " + (lin - 1) : ""), (col ? "columna: " + col : ""))
- console.error(error)
-};
-window.console.clear =
- /** @param {any[]} parametros */
- (...parametros) => {
-  __cl.apply(null, parametros)
-  if (__pre) {
-   __pre.textContent = ""
-  }
- }
-window.console.log =
- /** @param {any[]} parametros */
- (...parametros) => {
-  __lo.apply(null, parametros);
-  const div = document.createElement("div")
-  div.textContent = parametros.join(" ")
-  if (__pre) {
-   __pre.append(div);
-  }
- };
-window.console.error =
- /** @param {any[]} parametros */
- (...parametros) => {
-  const div = document.createElement("div")
-  div.style.color = "red"
-  div.textContent = parametros.join(" ")
-  if (__pre) {
-   __pre.append(div)
-  }
-  __err.apply(null, parametros);
- }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ } catch(e) {console.error(e)}
+ // Programa terminado
 }
-/**
- * @param {string} mensaje
- */
- function question(mensaje) {
- const respuesta = prompt(mensaje)
- console.log(mensaje + respuesta)
- return respuesta || ""
-}
-/**
- * @param {string} mensaje
- */
-function questionInt(mensaje) {
- return parseInt(prompt(mensaje || "") || "", 10)
-}
-/**
- * @param {string} mensaje
- */
-function questionFloat(mensaje) {
- return parseFloat(prompt(mensaje || "") || "")
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Programa terminado
 </script>`)
 }

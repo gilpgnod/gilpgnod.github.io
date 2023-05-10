5a0c8df206d039796634628f357ba262d2eab7a7
gilpgdm <gilpgdm@gmail.com>
2023-05-10 16:43:38 -0600

    sw 1.03

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para depués
  * actualizar este archivo. */
 const CACHE = "gilpgnod";
-const VERSION = "1.02"
+const VERSION = "1.03"
 
 /** Archivos requeridos para que la aplicación funcione fuera de línea. */
 const ARCHIVOS = [
